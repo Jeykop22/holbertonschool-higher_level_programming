577f5c33da656f0e5b4df0b11624f24ef525a15f
Jeykop22 <nolanbinet2@gmail.com>
2024-02-05 19:24:24 +0000

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-create class square
+Define a class Square.
 """
 
 
@@ -12,11 +12,11 @@ class Square:
         return (self.__size)
 
     def size(self, value):
-        self.__size = value
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         return (self.__size * self.__size)
