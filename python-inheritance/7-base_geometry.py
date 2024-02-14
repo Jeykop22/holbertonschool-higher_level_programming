eae2f0eccbe833225d437a9c76feb4231d2bc1df
Jeykop22 <nolanbinet2@gmail.com>
2024-02-14 11:37:39 +0000

    7-base_geometry.py

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validate a parameter as an integer"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer.format(name)")
+        if value <= 0:
+            raise ValueError("{} must be greater than 0.format(name)")
+        self.value = value
