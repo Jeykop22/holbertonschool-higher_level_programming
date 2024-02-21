bf4a2fab8e125afab77460d66ccf02f42a9f6ad9
Jeykop22 <nolanbinet2@gmail.com>
2024-02-21 11:28:28 +0000

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,13 +8,18 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Manage the id attribute for all classes"""
+        """Manage the id attribute for all classes
+
+        Args:
+            id: id of the instance. Defaults to None.
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None or not list_dictionaries:
@@ -22,8 +27,19 @@ class Base:
         else:
             return json.dumps(list_dictionaries)
 
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None or not json_string:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
     def save_to_file(cls, list_objs):
-        """writes the JSON string representation of list_objs"""
+        """writes the JSON string representation of list_objs to a file
+        list_objs is a list of instances who inherits of Base
+        filename is <class name>.json"""
         filename = cls.__name__ + ".json"
         listOf = []
         if list_objs is not None:
@@ -32,13 +48,7 @@ class Base:
         with open(filename, 'w') as jsonFileSaved:
             jsonFileSaved.write(cls.to_json_string(listOf))
 
-    def from_json_string(json_string):
-        """returns the list of the JSON string representation json_string"""
-        if json_string is None or not json_string:
-            return []
-        else:
-            return json.loads(json_string)
-
+    @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
         if cls.__name__ == "Rectangle":
@@ -48,6 +58,7 @@ class Base:
         dummy.update(**dictionary)
         return dummy
 
+    @classmethod
     def load_from_file(cls):
         """ returns a list of instances from a json file"""
         filename = cls.__name__ + ".json"
